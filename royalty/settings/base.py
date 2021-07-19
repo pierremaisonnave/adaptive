2257371a048e639ede955e5686d11ffb802457a2
pierremaisonnave <78214628+pierremaisonnave@users.noreply.github.com>
2021-07-19 14:43:39 +0200

    initial comit

--- royalty/settings/base.py
+++ royalty/settings/base.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,12 +136,7 @@ USE_TZ = True
 
 
 
-
-
-
-
 #S3 BUCKETS CONFIG
-
 
 AWS_ACCESS_KEY_ID=os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.getenv('AWS_SECRET_ACCESS_KEY')
